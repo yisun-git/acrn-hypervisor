15f74f6d5773e2f3e3068f42f456e87abc48d715
Yi Sun <yi.y.sun@linux.intel.com>
2024-03-20 03:22:50 -0400

    ivshmem: Add comments for ivshmem
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>

--- hypervisor/dm/vpci/ivshmem.c
+++ hypervisor/dm/vpci/ivshmem.c
@@ -1,7 +1,29 @@
-/*
+/*-
  * Copyright (C) 2020-2022 Intel Corporation.
+ * All rights reserved.
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NETAPP, INC ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL NETAPP, INC OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 #ifdef CONFIG_IVSHMEM_ENABLED
@@ -14,30 +36,46 @@
 #include <ivshmem_cfg.h>
 #include "vpci_priv.h"
 
-/* config space of ivshmem device */
-#define	IVSHMEM_CLASS		0x05U
-#define	IVSHMEM_REV		0x01U
-
-/*
- * ivshmem device supports bar0, bar1 and bar2,
- * indexes of them shall begin with 0 and be continuous.
+/**
+ * @addtogroup vp-dm_vperipheral
+ *
+ * @{
  */
-#define IVSHMEM_MMIO_BAR	0U
-#define IVSHMEM_MSIX_BAR	1U
-#define IVSHMEM_SHM_BAR	2U
 
-#define IVSHMEM_MMIO_BAR_SIZE 256UL
+/**
+ * @file
+ * @brief This file declares ivshmem device functions, data structures and macros
+ *
+ * This file declares ivshmem device functions, data structures, and macros to handle shared memory events between VMs.
+ *
+ */
 
-/* The device-specific registers of ivshmem device */
-#define	IVSHMEM_IRQ_MASK_REG	0x0U
-#define	IVSHMEM_IRQ_STA_REG	0x4U
-#define	IVSHMEM_IV_POS_REG	0x8U
-#define	IVSHMEM_DOORBELL_REG	0xcU
+#define	IVSHMEM_CLASS		0x05U /**< Pre-defined class code of the ivshmem device. */
+#define	IVSHMEM_REV		0x01U /**< Pre-defined revision ID of the ivshmem device. */
+#define IVSHMEM_MMIO_BAR	0U /**< Pre-defined mmio bar of the ivshmem device. */
+#define IVSHMEM_MSIX_BAR	1U /**< Pre-defined msix bar of the ivshmem device. */
+#define IVSHMEM_SHM_BAR		2U /**< Pre-defined shared memory region bar of the ivshmem device. */
+#define IVSHMEM_MMIO_BAR_SIZE	256UL /**< Pre-defined mmio bar size of the ivshmem device. */
+#define	IVSHMEM_IRQ_MASK_REG	0x0U /**< Pre-defined interrupt mask register offset of the ivshmem device. */
+#define	IVSHMEM_IRQ_STA_REG	0x4U /**< Pre-defined interrupt status register offset of the ivshmem device. */
+#define	IVSHMEM_IV_POS_REG	0x8U /**< Pre-defined inter-vm position register offset of the ivshmem device. */
+#define	IVSHMEM_DOORBELL_REG	0xcU /**< Pre-defined doorbell register offset of the ivshmem device. */
 
+/**  The shared memory region list of ivshmem device */
 static struct ivshmem_shm_region mem_regions[8] = {
 	IVSHMEM_SHM_REGIONS
 };
 
+/**
+ * @brief Union of ivshmem device doorbell register
+ *
+ * The ivshmem device doorbell register value is kept in this union.
+ *
+ * @consistency N/A
+ * @alignment N/A
+ *
+ * @remark N/A
+ */
 union ivshmem_doorbell {
 	uint32_t val;
 	struct {
@@ -46,6 +84,17 @@ union ivshmem_doorbell {
 	} reg;
 };
 
+/**
+ * @brief Data structure to present ivshmem device
+ *
+ * The structure includes the ivshmem device information, the corresponding pci device, the ivshmem device
+ * registers and the shared memory regions.
+ *
+ * @consistency N/A
+ * @alignment N/A
+ *
+ * @remark N/A
+ */
 struct ivshmem_device {
 	struct pci_vdev* pcidev;
 	union {
@@ -66,8 +115,8 @@ struct ivshmem_device {
 	struct ivshmem_shm_region *region;
 };
 
-static struct ivshmem_device ivshmem_dev[IVSHMEM_DEV_NUM];
-static spinlock_t ivshmem_dev_lock = { .head = 0U, .tail = 0U, };
+static struct ivshmem_device ivshmem_dev[IVSHMEM_DEV_NUM]; /**< Array of ivshmem device */
+static spinlock_t ivshmem_dev_lock = { .head = 0U, .tail = 0U, }; /**< Spin lock used to create/deinit ivshmem device */
 
 void init_ivshmem_shared_memory()
 {
@@ -81,8 +130,18 @@ void init_ivshmem_shared_memory()
 	}
 }
 
-/*
+/**
+ * @brief Find shared memory region by name
+ *
+ * @param[in] name The name string used to find the shared memory region.
+ *
+ * @return The shared memory region found out by name. It could be NULL if no region found out.
+ *
  * @pre name != NULL
+ *
+ * @post N/A
+ *
+ * @remark N/A
  */
 static struct ivshmem_shm_region *find_shm_region(const char *name)
 {
@@ -97,12 +156,21 @@ static struct ivshmem_shm_region *find_shm_region(const char *name)
 }
 
 /*
- * @brief There are two ivshmem server implementation in HV-land and
- *	  DM-land, they're used for briding the notification channel
- *	  between ivshmem devices acrossed VMs.
+ * @brief Bind the ivshmem device to a shared memory region
+ *
+ * There are two ivshmem server implementation in HV-land and DM-land, they're used for briding the notification
+ * channel between ivshmem devices acrossed VMs.
+ *
+ * @param[in] vdev Pointer to the virtual PCI device to access.
+ *
+ * @return None
  *
  * @pre vdev != NULL
  * @pre region->doorbell_peers[vm_id] = NULL
+ *
+ * @post region->doorbell_peers[vm_id] != NULL
+ *
+ * @remark N/A
  */
 static void ivshmem_server_bind_peer(struct pci_vdev *vdev)
 {
@@ -121,7 +189,17 @@ static void ivshmem_server_bind_peer(struct pci_vdev *vdev)
 }
 
 /*
+ * @brief Unbind the ivshmem device from a shared memory region
+ *
+ * @param[in] vdev Pointer to the virtual PCI device to access.
+ *
+ * @return None
+ *
  * @pre vdev != NULL
+ *
+ * @post region->doorbell_peers[vpci2vm(vdev->vpci)->vm_id] == NULL
+ *
+ * @remark N/A
  */
 static void ivshmem_server_unbind_peer(struct pci_vdev *vdev)
 {
@@ -131,7 +209,22 @@ static void ivshmem_server_unbind_peer(struct pci_vdev *vdev)
 }
 
 /*
+ * @brief Inject MSI to target VM if the doorbell is triggered
+ *
+ * The fucntion is used to notify target VM to check shared memory when xource VM writes ivshmem
+ * device doorbell register.
+ *
+ * @param[in] src_ivs_dev  Pointer to source ivshmem device.
+ * @param[in] dest_peer_id The destination ID used to find the peer ivshmem device.
+ * @param[in] vector_index The vector index that need to be injected to destination VM.
+ *
+ * @return None
+ *
  * @pre src_ivs_dev != NULL
+ *
+ * @post N/A
+ *
+ * @remark N/A
  */
 static void ivshmem_server_notify_peer(struct ivshmem_device *src_ivs_dev, uint16_t dest_peer_id, uint16_t vector_index)
 {
@@ -163,7 +256,17 @@ static void ivshmem_server_notify_peer(struct ivshmem_device *src_ivs_dev, uint1
 }
 
 /*
+ * @brief Put the input vdev into the ivshmem_dev array
+ *
+ * @param[in] vdev Pointer to the virtual PCI device to access.
+ *
+ * @return None
+ *
+ * @pre vdev != NULL
+ *
  * @post vdev->priv_data != NULL
+ *
+ * @remark N/A
  */
 static void create_ivshmem_device(struct pci_vdev *vdev)
 {
@@ -187,7 +290,20 @@ static void create_ivshmem_device(struct pci_vdev *vdev)
 }
 
 /*
+ * @brief Handle mmio access from VM
+ *
+ * When VM accesses ivshmem device mmio, it traps and is handled in this function.
+ *
+ * @param[in] io_req Pointer to the mmio request.
+ * @param[in] data   Pointer to the virtual PCI device to access.
+ *
+ * @return 0
+ *
  * @pre vdev->priv_data != NULL
+ *
+ * @post vdev->priv_data != NULL
+ *
+ * @remark N/A
  */
 static int32_t ivshmem_mmio_handler(struct io_request *io_req, void *data)
 {
@@ -227,6 +343,21 @@ static int32_t ivshmem_mmio_handler(struct io_request *io_req, void *data)
 	return 0;
 }
 
+/*
+ * @brief Read ivshmem device config
+ *
+ * @param[in]  vdev   Pointer to the virtual PCI device to access.
+ * @param[in]  offset The offset of config space to read.
+ * @param[in]  bytes  The size of data to read.
+ * @param[out] val    Pointer to the buffer of the value read out from config space.
+ *
+ * @return 0
+ *
+ * @pre vdev != NULL
+ * @pre val != NULL
+ *
+ * @remark N/A
+ */
 static int32_t read_ivshmem_vdev_cfg(struct pci_vdev *vdev, uint32_t offset, uint32_t bytes, uint32_t *val)
 {
 	*val = pci_vdev_read_vcfg(vdev, offset, bytes);
@@ -234,6 +365,20 @@ static int32_t read_ivshmem_vdev_cfg(struct pci_vdev *vdev, uint32_t offset, uin
 	return 0;
 }
 
+/*
+ * @brief Unmap ivshmem device bar
+ *
+ * This function unmaps ivshmem device bar from EPT or unregister the mmio handler.
+ *
+ * @param[in] vdev Pointer to the virtual PCI device to access.
+ * @param[in] idx  The BAR ID to do unmap.
+ *
+ * @return None
+ *
+ * @pre vdev != NULL
+ *
+ * @remark N/A
+ */
 static void ivshmem_vbar_unmap(struct pci_vdev *vdev, uint32_t idx)
 {
 	struct acrn_vm *vm = vpci2vm(vdev->vpci);
@@ -247,8 +392,18 @@ static void ivshmem_vbar_unmap(struct pci_vdev *vdev, uint32_t idx)
 }
 
 /*
- * @pre vdev->priv_data != NULL
- * @pre msix->table_offset == 0U
+ * @brief Map ivshmem device bar
+ *
+ * This function maps ivshmem device bar to EPT or register the mmio handler and delete the bar from EPT.
+ *
+ * @param[in] vdev Pointer to the virtual PCI device to access.
+ * @param[in] idx  The BAR ID to do map.
+ *
+ * @return None
+ *
+ * @pre vdev != NULL
+ *
+ * @remark N/A
  */
 static void ivshmem_vbar_map(struct pci_vdev *vdev, uint32_t idx)
 {
@@ -270,6 +425,20 @@ static void ivshmem_vbar_map(struct pci_vdev *vdev, uint32_t idx)
 	}
 }
 
+/*
+ * @brief Write ivshmem device config
+ *
+ * @param[in,out] vdev   Pointer to the virtual PCI device to access.
+ * @param[in]     offset The offset of config space to write.
+ * @param[in]     bytes  The size of data to write.
+ * @param[in]     val    The value to be written into config space.
+ *
+ * @return 0
+ *
+ * @pre vdev != NULL
+ *
+ * @remark N/A
+ */
 static int32_t write_ivshmem_vdev_cfg(struct pci_vdev *vdev, uint32_t offset, uint32_t bytes, uint32_t val)
 {
 	if (vbar_access(vdev, offset)) {
@@ -285,8 +454,17 @@ static int32_t write_ivshmem_vdev_cfg(struct pci_vdev *vdev, uint32_t offset, ui
 }
 
 /*
+ * @brief Initialize ivshmem device bar
+ *
+ * @param[in,out] vdev    Pointer to the virtual PCI device to access.
+ * @param[in]     bar_idx The BAR ID to initialize.
+ *
+ * @return None
+ *
  * @pre vdev != NULL
  * @pre bar_idx < PCI_BAR_COUNT
+ *
+ * @remark N/A
  */
 static void init_ivshmem_bar(struct pci_vdev *vdev, uint32_t bar_idx)
 {
@@ -327,6 +505,19 @@ static void init_ivshmem_bar(struct pci_vdev *vdev, uint32_t bar_idx)
 	}
 }
 
+/*
+ * @brief Initialize an ivshmem device
+ *
+ * This function creates an ivshmem device, initialize its config space and bars, and bind it to a shared memory region.
+ *
+ * @param[in,out] vdev Pointer to the virtual PCI device to access.
+ *
+ * @return None
+ *
+ * @pre vdev != NULL
+ *
+ * @remark N/A
+ */
 static void init_ivshmem_vdev(struct pci_vdev *vdev)
 {
 	create_ivshmem_device(vdev);
@@ -351,7 +542,16 @@ static void init_ivshmem_vdev(struct pci_vdev *vdev)
 }
 
 /*
+ * @brief Deinit an ivshmem device
+ *
+ * @param[in] vdev Pointer to the virtual PCI device to access.
+ *
+ * @return None
+ *
+ * @pre vdev != NULL
  * @pre vdev->priv_data != NULL
+ *
+ * @remark N/A
  */
 static void deinit_ivshmem_vdev(struct pci_vdev *vdev)
 {
@@ -366,10 +566,6 @@ static void deinit_ivshmem_vdev(struct pci_vdev *vdev)
 	spinlock_release(&ivshmem_dev_lock);
 }
 
-/**
- * @pre vm != NULL
- * @pre dev != NULL
- */
 int32_t create_ivshmem_vdev(struct acrn_vm *vm, struct acrn_vdev *dev)
 {
 	uint32_t i;
@@ -412,6 +608,9 @@ int32_t destroy_ivshmem_vdev(struct pci_vdev *vdev)
 	return 0;
 }
 
+/**
+ * @brief A set of callback functions used to operate the ivshmem device.
+ */
 const struct pci_vdev_ops vpci_ivshmem_ops = {
 	.init_vdev	= init_ivshmem_vdev,
 	.deinit_vdev	= deinit_ivshmem_vdev,
--- hypervisor/include/dm/ivshmem.h
+++ hypervisor/include/dm/ivshmem.h
@@ -1,24 +1,76 @@
-/*
+/*-
  * Copyright (C) 2020-2022 Intel Corporation.
+ * All rights reserved.
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NETAPP, INC ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL NETAPP, INC OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 #ifndef IVSHMEM_H
 #define IVSHMEM_H
 
-#define	IVSHMEM_VENDOR_ID	0x1af4U
-#define	IVSHMEM_DEVICE_ID	0x1110U
+/**
+ * @addtogroup vp-dm_vperipheral
+ *
+ * @{
+ */
+
+/**
+ * @file
+ * @brief This file declares ivshmem device external APIs provided by the virtual PCI (vPCI) component.
+ *
+ * This file declares ivshmem device external functions, data structures, and macros that shall be provided by the vPCI
+ * component in vp-dm.vperipheral module.
+ *
+ */
+
+#define	IVSHMEM_VENDOR_ID	0x1af4U /**< Pre-defined "Vendor ID" of ivshmem device */
+#define	IVSHMEM_DEVICE_ID	0x1110U /**< Pre-defined "Device ID" of ivshmem device */
+
 #ifdef CONFIG_IVSHMEM_ENABLED
 
-/*
+/**
+ * @brief Pre-defined max number of peers for each ivshmem region
+ *
  * Max number of peers for each ivshmem region, and
  * VM ID is used as peer IDs of this VM's ivshmem devices.
  */
 #define MAX_IVSHMEM_PEER_NUM (CONFIG_MAX_VM_NUM)
 
-/* Max number of MSIX table entries of shmem device. */
-#define MAX_IVSHMEM_MSIX_TBL_ENTRY_NUM 8U
+#define MAX_IVSHMEM_MSIX_TBL_ENTRY_NUM 8U /**< Pre-defined max number of MSIX table entries of shmem device. */
+
+/**
+ * @brief Data structure to present ivshmem device shared memory region information
+ *
+ * For a ivshmem device shared memory region, this structure includes its name,
+ * HPA of region, region size and the array to keep the ivshmem devices of the
+ * region.
+ *
+ * @consistency N/A
+ * @alignment N/A
+ *
+ * @remark N/A
+ */
 struct ivshmem_shm_region {
 	char name[32];
 	uint64_t hpa;
@@ -32,11 +84,51 @@ extern const struct pci_vdev_ops vpci_ivshmem_ops;
  * @brief Initialize ivshmem shared memory regions
  *
  * Initialize ivshmem shared memory regions based on user configuration.
+ *
+ * @return None
+ *
+ * @pre N/A
+ *
+ * @post N/A
+ *
+ * @remark N/A
  */
 void init_ivshmem_shared_memory(void);
 
+/*
+ * @brief Create an ivshmem device for a VM
+ *
+ * This function creates an ivshmem device as vPCI device for a VM and initialize it.
+ *
+ * @param[in,out] vm  Pointer to VM to create ivshmem device.
+ * @param[in]     dev Pointer to virtual device that the VM wants to create.
+ *
+ * @return 0 if no error happens otherwise return -EINVAL.
+ *
+ * @pre vm != NULL
+ * @pre dev != NULL
+ *
+ * @remark N/A
+ */
 int32_t create_ivshmem_vdev(struct acrn_vm *vm, struct acrn_vdev *dev);
+
+/*
+ * @brief Destroy an ivshmem device
+ *
+ * @param[in,out] vdev Pointer to the virtual PCI device to destroy.
+ *
+ * @return 0
+ *
+ * @pre vdev != NULL
+ *
+ * @remark N/A
+ */
 int32_t destroy_ivshmem_vdev(struct pci_vdev *vdev);
+
 #endif /* CONFIG_IVSHMEM_ENABLED */
+
+/**
+ * @}
+ */
 
 #endif /* IVSHMEM_H */
